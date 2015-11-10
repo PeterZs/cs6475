703104f253ae659e35a1a6c1fa2cf034e7823537
Ngoc(Amy) Tran <amy.tran@gatech.edu>
2015-11-10 14:58:16 -0800

    Added commited files for assignment 8

--- assignment8/assignment8.py
+++ assignment8/assignment8.py
@@ -1,5 +1,5 @@
 # ASSIGNMENT 8
-# Your Name
+# Ngoc(Amy) Tran
 
 import numpy as np
 import scipy as sp
@@ -66,9 +66,12 @@ def getImageCorners(image):
     """
     corners = np.zeros((4, 1, 2), dtype=np.float32)
     # WRITE YOUR CODE HERE
+    length, width = image.shape[:2]
 
-
-
+    corners[0] = np.array([[0, 0]])
+    corners[1] = np.array([[width, 0 ]])
+    corners[2] = np.array([[0, length ]])
+    corners[3] = np.array([[width, length]])
 
     return corners
     # END OF FUNCTION
@@ -118,10 +121,20 @@ def findMatchesBetweenImages(image_1, image_2, num_matches):
     image_2_desc = None
 
     # COPY YOUR CODE FROM A7 HERE.
-
-
-
-
+    #Initial SIFT detector
+    orb = cv2.ORB()
+    # find the keypoints and descriptors with SIFT
+    image_1_kp, image_1_desc = orb.detectAndCompute(image_1,None)
+    image_2_kp, image_2_desc = orb.detectAndCompute(image_2,None)
+    # create BFMatcher object
+    bfMacth =  cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    # Match descriptors.
+    matches = bfMacth.match(image_1_desc, image_2_desc)
+    # Sort the matches based on distances
+    matches = sorted(matches, key = lambda x:x.distance)
+    # return the top 'num_matches' matches in a list
+    if len(matches) >= num_matches:
+        matches = matches[:num_matches]
 
     return image_1_kp, image_2_kp, matches
   # END OF FUNCTION.
@@ -165,12 +178,14 @@ def findHomography(image_1_kp, image_2_kp, matches):
     image_2_points = np.zeros((len(matches), 1, 2), dtype=np.float32)
 
     # WRITE YOUR CODE HERE.
+    for i, match in enumerate(matches):
+        image_1_points[i] = image_1_kp[match.queryIdx].pt
+        image_2_points[i] = image_2_kp[match.trainIdx].pt
 
+    homography, mask = cv2.findHomography(image_1_points, image_2_points, method=cv2.RANSAC, ransacReprojThreshold=5.0)
 
-
-
-    # Replace this return statement with the homography.
-    return None
+    # Ignore the mask, and simply return the homography.
+    return homography
     # END OF FUNCTION
 
 def blendImagePair(warped_image, image_2, point):
@@ -197,13 +212,43 @@ def blendImagePair(warped_image, image_2, point):
     Returns:
         image: The warped image with image_2 blended into it.
     """
-    output_image = np.copy(warped_image)
-    # REPLACE THIS WITH YOUR BLENDING CODE.
-    output_image[point[1]:point[1] + image_2.shape[0],
-                 point[0]:point[0] + image_2.shape[1]] = image_2
+    # created an array of zeros  
+    image_2_array_size = np.zeros(warped_image.shape, dtype=np.uint8)
+    # ensure created a properly size 
+    image_2_array_size[point[1]:image_2.shape[0]+point[1], \
+                   point[0]:image_2.shape[1]+point[0]] = image_2
 
+ 
+    #Convert images to GRAY color space
+    warp_in_grey = cv2.cvtColor(warped_image, cv2.COLOR_BGR2GRAY)
+    image2_in_grey = cv2.cvtColor(image_2_array_size, cv2.COLOR_BGR2GRAY)
+    # Compute the mask of two images using logical NOT
+    warp_mask = 1 - np.logical_not(warp_in_grey)
+    image2_mask = 1 - np.logical_not(image2_in_grey)
+    # calculate union of the two images using logical AND
+    union_mask = np.logical_and(warp_in_grey, image2_in_grey)
 
+    # Use sum of union_mask to get the extents of uion
+    col_sum = np.sum(union_mask, axis = 0)   
+    idx_end = np.where(col_sum != 0)[0][-1]
+    idx_start = np.where(col_sum != 0)[0][0]
 
+    #Create a img_weight zero array image
+    img_weight = np.zeros(warp_in_grey.shape, dtype=np.float32)
+
+    for i, col in enumerate(union_mask.T[idx_start:idx_end]):
+        img_weight[:,idx_start+i] = col * (1 - i / float(idx_end-idx_start))
+
+    # mask for image_2
+    image2_mask = image2_mask.astype(np.float32) - img_weight
+
+    # Blend image
+    output_image = np.zeros(warped_image.shape, dtype=np.uint8)
+    if len(warped_image.shape) == 3:
+        for i in range(warped_image.shape[2]):
+            output_image[:,:,i] = (1 - image2_mask) * warped_image[:,:,i] + image2_mask * image_2_array_size[:,:,i]
+    else:
+        output_image = (1 - image2_mask) * warped_image + image2_mask * image_2_array_size
 
     return output_image
     # END OF FUNCTION
@@ -270,9 +315,47 @@ def warpImagePair(image_1, image_2, homography):
     y_max = 0
 
     # WRITE YOUR CODE HERE
+    #*1.Obtain the corners for image 1 and image 2
+    # using the function getImageCorners
+    image_1_corners = getImageCorners(image_1)
+    image_2_corners = getImageCorners(image_2)
 
+    #*2.Transform the perspective of the corners of image 1
+    image_1_corners = cv2.perspectiveTransform(image_1_corners, homography)
 
+    #*3.Join the two corner arrays together
+    joined = np.concatenate((image_1_corners, image_2_corners))
 
+    #*4.For the first column of this array, find the min and max.
+    # This willbe minimum and maximum X values.
+    # Store into x_min, x_max.
+    left = joined[:, :, :1]
+    x_min = np.min(left)
+    x_max = np.max(left)
+
+    #*5.For the second column of this array, find the min and max.
+    # This willbe the minimum and maximum Y values.
+    # Store into y_min, y_max.
+    right = joined[:, :, 1:]
+    y_min = np.min(right)
+    y_max = np.max(right)
+
+    #*6.Create a translation matrix that will shift the image by the required
+    #x_min and y_min (should be a numpy.ndarray)
+    translation = np.array(
+                        [[1, 0, -1 * x_min],
+                        [0, 1, -1 * y_min],
+                        [0, 0, 1]])
+    #*7.Compute the dot product of your translation matrix & homography
+    #use np.dor to product of two arrays.
+    dotProduct = np.dot(translation, homography)
+
+    #*8.Then call cv2.warpPerspective.
+    vector = (x_max - x_min, y_max - y_min)
+    warped_image = cv2.warpPerspective(image_1, dotProduct, vector)
+
+    #*9.To finish, you need to blend both images.
+    # We have coded the call to the blend function for you.
 
     # END OF CODING
     output_image = blendImagePair(warped_image, image_2,
@@ -280,10 +363,13 @@ def warpImagePair(image_1, image_2, homography):
     return output_image
 
 # Some simple testing.
-# image_1 = cv2.imread("images/source/panorama_1/1.jpg")
-# image_2 = cv2.imread("images/source/panorama_1/2.jpg")
-# image_1_kp, image_2_kp, matches = findMatchesBetweenImages(image_1, image_2,
-#                                                            20)
-# homography = findHomography(image_1_kp, image_2_kp, matches)
-# result = warpImagePair(image_1, image_2, homography)
-# cv2.imwrite("images/output/panorama_1_result.jpg", result)
+#image_1 = cv2.imread("images/source/panorama_1/amy_1.jpg")
+#image_2 = cv2.imread("images/source/panorama_1/amy_2.jpg")
+#image_1 = cv2.imread("images/source/panorama_1/amy_1_2_result.jpg")
+#image_2 = cv2.imread("images/source/panorama_1/amy_3.jpg")
+
+#image_1_kp, image_2_kp, matches = findMatchesBetweenImages(image_1, image_2,
+#                                                          20)
+#homography = findHomography(image_1_kp, image_2_kp, matches)
+#result = warpImagePair(image_1, image_2, homography)
+#cv2.imwrite("images/output/amy_1_3_result.jpg", result)
